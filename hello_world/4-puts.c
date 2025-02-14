c8f6984f670f553d022ee2e985b3a1e11c604ea5
Steph974-Git <10466@holbertonstudents.com>
2025-02-14 09:35:59 +0000

    Write a C program that prints

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*/
+ * main -Entry
+ * Description: Prints a string using puts
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
