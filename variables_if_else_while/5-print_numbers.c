0178ed80c95aa0be543379ce07301ad270d20730
Steph974-Git <10466@holbertonstudents.com>
2025-02-17 15:03:22 +0000

    digit numbers of base 10 starting from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print  the number stored in the variable n
+ *
+ * Return: Always (Success)
+ */
+
+	int main(void)
+{
+	int number;
+
+	for (number = 0 ; number < 10 ; number++)
+{
+	printf("%d", number);
+}
+	printf("\n");
+
+	return (0);
+}
