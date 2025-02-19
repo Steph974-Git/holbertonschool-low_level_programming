f2f67f5a0e39b69d6dca9cc9f446be2a958aa326
Steph974-Git <10466@holbertonstudents.com>
2025-02-19 21:47:10 +0000

    adds two integers and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first number
+ * @b: second number
+ * Return:  the absolute value
+ */
+
+int add(int a, int b)
+
+{
+	int resul = a + b;
+
+	return abs(resul);
+}
+
