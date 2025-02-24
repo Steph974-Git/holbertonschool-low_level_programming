cec2e6e9cf24b9eb9db785dcf6d5741008aea62a
Steph974-Git <10466@holbertonstudents.com>
2025-02-24 21:11:55 +0000

    prints a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints a square
+ * @size:  is the size of the square
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+
+{
+	int idx1, idx2;
+
+	if (size <= 0)
+{
+	_putchar('\n');
+}
+	for (idx1 = 0; idx1 < size; idx1++)
+{
+	for (idx2 = 0; idx2 < size; idx2++)
+{
+	_putchar('#');
+}
+	_putchar('\n');
+}
+}
