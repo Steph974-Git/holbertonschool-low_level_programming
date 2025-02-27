3cd4a6a3ba9f9d71443232af62441d5709028d5b
Steph974-Git <10466@holbertonstudents.com>
2025-02-27 15:36:11 +0100

    Try1 task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy -   string pointed to by src
+ * @dest: the pointer
+ * @src:  string pointed
+ * Return: \n
+ */
+
+ char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", a[i]);
+	if (i < n - 1)
+	{
+	printf(", ");
+	}
+}
+	printf("\n");
+}
