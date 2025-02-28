e26f9f9d47b39069652eff68071fa77c4e8f1207
Steph974-Git <10466@holbertonstudents.com>
2025-02-28 15:03:34 +0100

    capitalizes all words of a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *cap_string -  changes all
+ * @str:  all lowercase letters
+ * Return: str
+ */
+
+char *cap_string(char *str)
+
+{
+	int i = 0;
+	size_t j;
+	int cap = 1;
+	char arr[14] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}',
+			' ', '\n', '\t'};
+
+	while (str[i] != '\0')
+	{
+	for (j = 0; j < sizeof(arr) / sizeof(arr[0]); j++)
+{
+		if (str[i] == arr[j])
+{
+		cap = 1;
+		break;
+}
+}
+	if (cap == 1 && str[i] >= 'a' && str[i] <= 'z')
+{
+		str[i] = str[i] - 32;
+		cap = 0;
+}
+	i++;
+}
+	return (str);
+}
+
