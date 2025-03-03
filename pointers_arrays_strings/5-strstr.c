68554b751266a28def2c3c91c9b35f082ea133b7
Steph974-Git <10466@holbertonstudents.com>
2025-03-03 21:10:45 +0100

    task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* *_strstr -   locates a substring
+* @haystack:  the string 
+* @needle: the first occurrence of the substring
+* Return: null
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+    if (*needle == '\0')
+        return (haystack);
+
+while (*haystack)
+	{
+	char *hay = haystack;
+    char *nee = needle;
+		while (*hay && *nee && *hay == *nee)
+        {
+            hay++;
+            nee++;
+        }
+		if (*nee == '\0')
+		return (haystack);
+	haystack++;
+    }
+	return (NULL);
+}
