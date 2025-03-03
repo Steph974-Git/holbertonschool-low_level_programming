d85a268de82a7bf4a0c7e3bd12860cef43eab138
Steph974-Git <10466@holbertonstudents.com>
2025-03-03 23:28:09 +0100

    betty

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
- * *_memset - fills memory with a constant byte
- * @b: The constant byte to fill the memory with
- * @s: Pointer to the memory area to be filled
- * @n: The number of bytes to be set
- *  Return : Pointer to the memory area s
- */
-
+* *_memset - its my main.c with prototype into
+* @b: The constant byte to fill the memory with
+* @s: Pointer to the memory area to be filled
+* @n: The number of bytes to be set
+* Return: value on variable s
+*/
 char *_memset(char *s, char b, unsigned int n)
+
 {
 unsigned int i = 0;
-while (i < n)
-{
-s[i] = b;
-i++;
-}
-return (s);
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
- * *_memcpy - fills memory with a constant byte
- * @dest: The constant byte to fill the memory with
- * @src: Pointer to the memory area to be filled
- * @n: The number of bytes to be set
- *  Return : Pointer to the memory area s
- */
+* *_memcpy - fills memory with a constant byte
+* @dest: The constant byte to fill the memory with
+* @src: Pointer to the memory area to be filled
+* @n: The number of bytes to be set
+* Return: Pointer to the memory area s
+*/
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
-while (i < n)
-{
-dest[i] = src[i];
-i++;
-}
-return (dest);
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
 }
