d8ef3482395e7d0b153c6557ec2347bb9bc5270a
Steph974-Git <10466@holbertonstudents.com>
2025-03-05 15:40:02 +0100

    task argc, argv

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point of the program
- * @argc: The number of arguments passed to the program
- * @argv: The array of argument strings
+ * main - Programme qui multiplie deux nombres passés en arguments
+ * @argc: Nombre d'arguments passés au programme
+ * @argv: Tableau contenant les arguments sous forme de chaînes de caractères
  *
+ * Return: 0 si l'exécution est réussie, 1 en cas d'erreur
  */
 
 int main(int argc, char *argv[])
 
 {
 	int num1;
-        int num2;
+	int num2;
 	int result;
 
 	if (argc != 3)
