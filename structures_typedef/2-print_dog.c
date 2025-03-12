aa29214177e391775fc577bc8b714e4e74e02698
Steph974-Git <10466@holbertonstudents.com>
2025-03-12 10:40:47 +0100

    task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Affiche les informations d'une structure de type struct dog
+ * @d: Pointeur vers la structure à afficher
+ * Si le pointeur 'd' est NULL, la fonction ne fait rien.
+ * Si un attribut de la structure est NULL, il est affiché comme "(nil)".
+ * Sinon, la valeur de l'attribut est affichée.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("name: %s\n", d->name);
+	printf("age: %.6f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("owner: (nil)\n");
+	else
+		printf("owner: %s\n", d->owner);
+}
