06bb7c0d5e3b223fc52bd236e598638b330ff9d4
Steph974-Git <10466@holbertonstudents.com>
2025-03-12 09:37:34 +0100

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - Structure représentant un chien
+ * @name: Nom du chien (chaîne de caractères)
+ * @age: Âge du chien (nombre décimal)
+ * @owner: Nom du propriétaire (chaîne de caractères)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
