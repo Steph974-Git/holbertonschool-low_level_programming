5d9518d5262d822fe0f153e59858919a5abab3c4
Steph974-Git <10466@holbertonstudents.com>
2025-03-17 15:54:13 +0100

    Implement basic calculator functions and operator selection

---
+++ function_pointers/3-main.c
@@ -0,0 +1,19 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+int main(int argc, int argv[])
+{
+	int num1, num2;
+	int operator;
+
+	
+
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * op_add - Retourne la somme de a et b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: La somme de a et b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Retourne la différence de a et b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: La différence de a et b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Retourne le produit de a et b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: Le produit de a et b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mul - Retourne le résultat de la division de a par b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: Le résultat de la division de a par b
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	}
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mul -  Retourne le reste de la division de a par b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: Le reste de la division de a par b
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
