3c8f39d692e49661cbe0b63629f70100097b9ed0
Steph974-Git <10466@holbertonstudents.com>
2025-03-17 16:05:14 +0100

    Refactor main function for calculator program to handle input validation and operator execution

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,17 +3,32 @@
 #include <stdlib.h>
 
 /**
- * 
- * 
- * 
- * 
+ * main - Entry point for calculator program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success
  */
-
-int main(int argc, int argv[])
+int main(int argc, char *argv[])
 {
+	int (*op_func)(int, int);
 	int num1, num2;
-	int operator;
 
-	
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", op_func(num1, num2));
+	return (0);
+	}
 }
