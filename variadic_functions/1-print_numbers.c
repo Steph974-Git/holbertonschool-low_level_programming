2d99340f1c816370b3cf64954ec4238eb6190a68
Steph974-Git <10466@holbertonstudents.com>
2025-03-18 15:01:08 +0100

    Add print_strings and print_all functions; update main files for testing

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,34 +1,28 @@
 #include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers followed by a new line
- * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
- *
- * Return: void
- */
+* print_numbers - prints numbers followed by a new line
+* @separator: string to be printed between numbers
+* @n: number of integers passed to the function
+* Return: void
+*/
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    if (n == 0)
-        return;
+	va_list args;
+	unsigned int i;
 
-    if (separator == NULL)
-        separator = "";
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(args, int));
-    va_end(args);
-    printf("\n");
-    }
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-        va_arg(args, int);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
 
-    va_end(args);
-    printf ('\n');
-
+	va_end(args);
+	printf("\n");
 }
