feff2549ef1496386142edad016eb0ebfb158478
Steph974-Git <10466@holbertonstudents.com>
2025-04-01 14:31:21 +0200

    Add memory allocation for new node in insert_dnodeint_at_index function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -29,6 +29,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (curr->next == NULL)
 		return (add_dnodeint_end(h, n));
 
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
 	if (curr == NULL)
 		return (NULL);
 
