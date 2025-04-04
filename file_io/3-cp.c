7ecb29f3ba95dc6a5884e50a63cecf4e5e95297e
Steph974-Git <10466@holbertonstudents.com>
2025-04-04 12:46:46 +0200

    Refactor error handling in file copy logic to ensure proper resource management

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -39,12 +39,6 @@ void check_args(int argc, char *argv[], int *fd_from, int *fd_to)
 	}
 
 	*fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (*fd_to == -1)
-	{
-		close(*fd_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
 }
 
 /**
@@ -69,7 +63,6 @@ int main(int argc, char *argv[])
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
-			close(fd_to);
 			exit(99);
 		}
 	}
@@ -78,7 +71,8 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
-		close(fd_to);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(98);
 	}
 
