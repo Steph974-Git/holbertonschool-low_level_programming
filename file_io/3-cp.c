c0518d61e8f71f53ad4ad497d0cbbd83b34533bf
Steph974-Git <10466@holbertonstudents.com>
2025-04-04 12:09:57 +0200

    Refactor argument checking and error handling in file copy logic for improved clarity and safety

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,38 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
-
-/**
- * error_exit - handles error messages and exits
- * @code: exit code
- * @message: error message format
- * @arg: argument for message
- */
-void error_exit(int code, char *message, char *arg)
-{
-dprintf(STDERR_FILENO, message, arg);
-exit(code);
-}
-
-/**
- *  _strcmp  - compares two strings
- * @s1: first string
- * @s2: second string
- * Return: 1
- */
-
-int _strcmp(char *s1, char *s2)
-
-{
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
-{
-	s1++;
-	s2++;
-}
-
-	return (*s1 - *s2);
-}
 
 /**
  * close_fd - safely closes a file descriptor
@@ -49,37 +17,34 @@ exit(100);
 }
 
 /**
- * read_and_write - reads from source and writes to destination
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * @buffer: buffer for reading/writing
- * @argv: program arguments
- * Return: 1 if more to read, 0 if done
+ * check_args - checks arguments and opens files
+ * @argc: argument count
+ * @argv: argument vector
+ * @fd_from: pointer to source file descriptor
+ * @fd_to: pointer to destination file descriptor
  */
-int read_and_write(int fd_from, int fd_to, char *buffer, char *argv[])
+void check_args(int argc, char *argv[], int *fd_from, int *fd_to)
 {
-ssize_t bytes_read, bytes_written;
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
 
-bytes_read = read(fd_from, buffer, 1024);
-if (bytes_read == -1)
+*fd_from = open(argv[1], O_RDONLY);
+if (*fd_from == -1)
 {
-close(fd_from);
-if (fd_to != -1)
-close(fd_to);
-error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
-if (bytes_read > 0)
+
+*fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (*fd_to == -1)
 {
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written < 0 || bytes_written != bytes_read)
-{
-close(fd_from);
-close(fd_to);
-error_exit(99, "Error: Can't write to %s\n", argv[2]);
+close(*fd_from);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
 }
-return (1);
-}
-return (0);
 }
 
 /**
@@ -90,29 +55,31 @@ return (0);
  */
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to;
+int fd_from = -1, fd_to = -1;
+ssize_t bytes_read, bytes_written;
 char buffer[1024];
-int more_to_read = 1;
 
-if (argc != 3)
-error_exit(97, "Usage: cp file_from file_to\n", NULL);
+check_args(argc, argv, &fd_from, &fd_to);
 
-if (_strcmp(argv[1], argv[2]) == 0)
-error_exit(99, "Error: Can't write to %s\n", argv[2]);
-
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
-error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
-fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fd_to == -1)
+while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 {
+bytes_written = write(fd_to, buffer, bytes_read);
+if (fd_to == -1 || bytes_written != bytes_read)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 close(fd_from);
-error_exit(99, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
 }
 
-while (more_to_read)
-more_to_read = read_and_write(fd_from, fd_to, buffer, argv);
+if (bytes_read == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+close(fd_from);
+if (fd_to != -1)
+close(fd_to);
+exit(98);
+}
 
 close_fd(fd_from);
 close_fd(fd_to);
