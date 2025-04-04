3454e9ac2a244a4c633d8aff1388ed7c69ad96ce
Steph974-Git <10466@holbertonstudents.com>
2025-04-04 10:30:39 +0200

    Refactor error handling in 3-cp.c for improved clarity and maintainability

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,55 +1,51 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
 #include <stdlib.h>
 
 /**
- *  _strcmp  - compares two strings
- * @s1: first string
- * @s2: second string
- * Return: difference between strings
+ * error_handler - handle error messages and exit
+ * @code: exit code
+ * @message: error message format
+ * @arg: argument to insert in message
+ * @fd_from: file descriptor for source file
+ * @fd_to: file descriptor for destination file
  */
-int _strcmp(char *s1, char *s2)
+void error_handler(int code, char *message, char *arg, int fd_from, int fd_to)
 {
-	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	if (fd_from > 0)
+		close(fd_from);
+	if (fd_to > 0)
+		close(fd_to);
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
+}
+
+/**
+ * close_files - close file descriptors
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * Return: 0 on success
+ */
+int close_files(int fd_from, int fd_to)
+{
+	if (close(fd_from) == -1)
 	{
-		s1++;
-		s2++;
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
 	}
-
-	return (*s1 - *s2);
-}
-
-/**
- * error_exit - prints error message with string arg and exits
- * @code: exit code
- * @message: error message format
- * @arg: string argument to insert in message
- */
-void error_exit(int code, char *message, char *arg)
-{
-	dprintf(STDERR_FILENO, message, arg);
-	exit(code);
-}
-
-/**
- * error_exit_int - prints error message with int arg and exits
- * @code: exit code
- * @message: error message format
- * @arg: integer argument to insert in message
- */
-void error_exit_int(int code, char *message, int arg)
-{
-	dprintf(STDERR_FILENO, message, arg);
-	exit(code);
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
 }
 
 /**
  * main - copies the content of a file to another file
  * @argc: argument count
  * @argv: argument vector
- *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
@@ -59,39 +55,27 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", NULL);
-	if (_strcmp(argv[1], argv[2]) == 0)
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		error_handler(97, "Usage: cp file_from file_to\n", NULL, -1, -1);
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		error_handler(98, "Error: Can't read from file %s\n", argv[1], -1, -1);
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
-	}
-	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Can't write to %s\n", argv[2]);
-		}
-	}
-	if (bytes_read == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	}
-	if (close(fd_from) == -1)
-		error_exit_int(100, "Error: Can't close fd %d\n", fd_from);
+		error_handler(99, "Error: Can't write to %s\n", argv[2], fd_from, -1);
 
-	if (close(fd_to) == -1)
-		error_exit_int(100, "Error: Can't close fd %d\n", fd_to);
-	return (0);
+while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+{
+	bytes_written = write(fd_to, buffer, bytes_read);
+	if (bytes_written == -1)
+		error_handler(99, "Error: Can't write to %s\n",
+		argv[2], fd_from, fd_to);
+}
+
+	if (bytes_read == -1)
+		error_handler(98, "Error: Can't read from file %s\n",
+		argv[1], fd_from, fd_to);
+
+return (close_files(fd_from, fd_to));
 }
