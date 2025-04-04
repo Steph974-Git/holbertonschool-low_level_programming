3b05d09fadd7986f2d895cfb8a7c1e8f441506e6
Steph974-Git <10466@holbertonstudents.com>
2025-04-04 11:56:07 +0200

    Refactor read_and_write function in 3-cp.c for improved error handling and clarity

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 /**
  * error_exit - handles error messages and exits
@@ -34,7 +35,7 @@ exit(100);
  * @fd_to: destination file descriptor
  * @buffer: buffer for reading/writing
  * @argv: program arguments
- * Return: 0 on success
+ * Return: 1 if more to read, 0 if done
  */
 int read_and_write(int fd_from, int fd_to, char *buffer, char *argv[])
 {
@@ -51,7 +52,7 @@ error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 if (bytes_read > 0)
 {
 bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written != bytes_read)
+if (bytes_written < 0 || bytes_written != bytes_read)
 {
 close(fd_from);
 close(fd_to);
@@ -63,21 +64,6 @@ return (0);
 }
 
 /**
- * copy_file - copies content from source to destination
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * @argv: program arguments
- */
-void copy_file(int fd_from, int fd_to, char *argv[])
-{
-char buffer[1024];
-int more_to_read = 1;
-
-while (more_to_read)
-more_to_read = read_and_write(fd_from, fd_to, buffer, argv);
-}
-
-/**
  * main - entry point for cp program
  * @argc: argument count
  * @argv: argument vector
@@ -86,9 +72,14 @@ more_to_read = read_and_write(fd_from, fd_to, buffer, argv);
 int main(int argc, char *argv[])
 {
 int fd_from, fd_to;
+char buffer[1024];
+int more_to_read = 1;
 
 if (argc != 3)
 error_exit(97, "Usage: cp file_from file_to\n", NULL);
+
+if (strcmp(argv[1], argv[2]) == 0)
+error_exit(99, "Error: Can't write to %s\n", argv[2]);
 
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
@@ -101,7 +92,8 @@ close(fd_from);
 error_exit(99, "Error: Can't write to %s\n", argv[2]);
 }
 
-copy_file(fd_from, fd_to, argv);
+while (more_to_read)
+more_to_read = read_and_write(fd_from, fd_to, buffer, argv);
 
 close_fd(fd_from);
 close_fd(fd_to);
