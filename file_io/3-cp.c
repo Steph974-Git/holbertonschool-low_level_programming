7c72557eb1ad28f477d335f13db5c5debe94995e
Steph974-Git <10466@holbertonstudents.com>
2025-04-04 12:16:45 +0200

    Refactor error handling in file copy logic for improved clarity and consistency

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,11 +9,11 @@
  */
 void close_fd(int fd)
 {
-if (close(fd) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 
 /**
@@ -25,26 +25,26 @@ exit(100);
  */
 void check_args(int argc, char *argv[], int *fd_from, int *fd_to)
 {
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-*fd_from = open(argv[1], O_RDONLY);
-if (*fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
+	*fd_from = open(argv[1], O_RDONLY);
+	if (*fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-*fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (*fd_to == -1)
-{
-close(*fd_from);
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
+	*fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (*fd_to == -1)
+	{
+		close(*fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 
 /**
@@ -55,33 +55,33 @@ exit(99);
  */
 int main(int argc, char *argv[])
 {
-int fd_from = -1, fd_to = -1;
-ssize_t bytes_read, bytes_written;
-char buffer[1024];
+	int fd_from = -1, fd_to = -1;
+	ssize_t bytes_read, bytes_written;
+	char buffer[1024];
 
-check_args(argc, argv, &fd_from, &fd_to);
+	check_args(argc, argv, &fd_from, &fd_to);
 
-while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
-{
-bytes_written = write(fd_to, buffer, bytes_read);
-if (fd_to == -1 || bytes_written != bytes_read)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-exit(99);
-}
-}
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
 
-if (bytes_read == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close(fd_from);
-if (fd_to != -1)
-close(fd_to);
-exit(98);
-}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
 
-close_fd(fd_from);
-close_fd(fd_to);
-return (0);
+	close_fd(fd_from);
+	close_fd(fd_to);
+	return (0);
 }
